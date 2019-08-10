e85efd21eddb295f050e7f2007782f6487a57619
skniazi <shameerniazi24@gmail.com>
2019-08-11 01:42:03 +0500

    Updates

---
+++ precache-manifest.c2c890153029f161ce9bf71cabbdfb58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33c7d467fad64c6018c1d5b41bf6a53",
+    "url": "/peoples-api/index.html"
+  },
+  {
+    "revision": "d7d3cfa4ea1f08932114",
+    "url": "/peoples-api/static/css/main.93c1d566.chunk.css"
+  },
+  {
+    "revision": "1171571eb199685b11b2",
+    "url": "/peoples-api/static/js/2.45c92687.chunk.js"
+  },
+  {
+    "revision": "d7d3cfa4ea1f08932114",
+    "url": "/peoples-api/static/js/main.9bfa4234.chunk.js"
+  },
+  {
+    "revision": "6654effe4dd242873ce8",
+    "url": "/peoples-api/static/js/runtime~main.8782336c.js"
+  }
+]);
